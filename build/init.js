0db1e999486bd19d5d715a983f68f925e46873b5
future <nagisz95@gmail.com>
2023-06-09 21:09:21 +0900

    for deployment

---
+++ build/init.js
@@ -0,0 +1,12 @@
+"use strict";
+
+require("dotenv/config");
+require("./db");
+require("./models/Video");
+var _server = _interopRequireDefault(require("./server"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var PORT = 4000;
+var handelListening = function handelListening() {
+  return console.log("\u2705 Server listening on port http://localhost:".concat(PORT));
+};
+_server["default"].listen(PORT, handelListening);
